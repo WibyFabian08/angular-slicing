8dec41b8587ff5f66875c98f6b7bd21e97101557
WibyFabian08 <wibyfabian08@gmail.com>
2022-08-22 13:57:19 +0700

    slicing register component

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm = new FormGroup({
+    name: new FormControl("", [ Validators.required]),
+    email: new FormControl("", [Validators.email, Validators.required]),
+    password: new FormControl("", [Validators.required]),
+  })
+  isVisible: boolean = false
+  eyeIcon: string = "../../../assets//icons/eye.png"
+  closeIcon: string = "../../../assets//icons/hide.png"
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleSubmit = () => {
+    console.log(this.registerForm.value)
+  }
+
+  handleShowPassword = () => {
+    this.isVisible = !this.isVisible
   }
 
 }
