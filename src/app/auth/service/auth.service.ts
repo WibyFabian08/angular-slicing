181a80e95eabdc5956035ad6f3269d2d607e5830
WibyFabian08 <wibyfabian08@gmail.com>
2022-08-22 14:11:05 +0700

    auth service

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../interfaces/user';
+import { Observable } from 'rxjs';
+
+const baseUrl = "http://localhost:3000"
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  register(data: any):Observable<User> {
+    return this.http.post<User>(`${baseUrl}/users`, data)
+  }
+  
+  login(email: any):Observable<User> {
+    return this.http.get<User>(`${baseUrl}/users?email=${email}`)
+  }
 }
